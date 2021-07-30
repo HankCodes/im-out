65baa5e77efdda47b88a432e21f9b9db62c7f0cd
Henrik <1565871@gmail.com>
2021-07-30 21:08:46 +0200

    added storage wrapper class

---
+++ imout/Core/Utils/StorageWrapper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Essentials;
+
+namespace imout.Core.Utils
+{
+    public class StorageWrapper
+    {
+        public string Get(string key)
+        {
+            return Preferences.Get(key, "");
+        }
+
+        public void Set(string key, string value)
+        {
+            Preferences.Set(key, value);
+        }
+    }
+}
