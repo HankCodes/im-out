9be9e8c3568df80daa8f11e9993936d572684e3c
Henrik <1565871@gmail.com>
2021-07-30 21:17:13 +0200

    added newtonsoft package and modified StorageWrapper

--- imout/Core/Utils/StorageWrapper.cs
+++ imout/Core/Utils/StorageWrapper.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using Xamarin.Essentials;
+﻿using Xamarin.Essentials;
 
 namespace imout.Core.Utils
 {
     public class StorageWrapper
     {
-        public string Get(string key)
+        public string Get(string key, string defaultValue)
         {
-            return Preferences.Get(key, "");
+            return Preferences.Get(key, defaultValue);
         }
 
         public void Set(string key, string value)
