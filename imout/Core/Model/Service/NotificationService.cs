51a71fef78cf0f3d3d60a0c3688952e457e24af1
Henrik <1565871@gmail.com>
2021-08-01 21:06:52 +0200

    started implementation of NotificationService

---
+++ imout/Core/Model/Service/NotificationService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using imout.Core.Model.Repository;
+
+namespace imout.Core.Model.Service
+{
+    public class NotificationService
+    {
+        private IRepository notificationRepo;
+
+        public NotificationService(IRepository notificationRepository)
+        {
+            notificationRepo = notificationRepository;
+        }
+
+        public void Create(Notification notification)
+        {
+            List<Notification> notifications = notificationRepo.Get();
+            notifications.ForEach(not => {
+                if (not.Id == notification.Id) throw new Exception("Notification already exists");
+            });
+
+            notifications.Add(notification);
+
+            notificationRepo.Set(notifications);
+        }
+
+        public void Update(Notification newNotification)
+        {
+            List<Notification> notifications = notificationRepo.Get();
+            Notification oldNotification = notifications.Find(not => not.Id == newNotification.Id);
+            if (oldNotification is null) throw new Exception("Notification does not exists");
+
+            notifications.Remove(oldNotification);
+            notifications.Add(newNotification);
+
+            notificationRepo.Set(notifications);
+
+        }
+
+        public void Delete(Notification notification)
+        {
+
+        }
+    }
+}
