be69e03623fe2ef16631158d55509196e86e0a83
Henrik <1565871@gmail.com>
2021-08-20 22:33:04 +0200

    implemented batterywrapper

---
+++ imout/Core/Utils/Battery/BatteryLevelChangedEventArgs.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace imout.Core.Utils.Battery
+{
+    public class BatteryLevelChangedEventArgs : EventArgs
+    {
+        public double Level { get; set; }
+        public bool IsCharging { get; set; }
+    }
+}
