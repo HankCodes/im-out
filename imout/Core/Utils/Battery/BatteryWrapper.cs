0bfd507483c0725fce577a64fae894689a4ecb79
Henrik <70313272+HankCodes@users.noreply.github.com>
2021-08-20 22:35:33 +0200

    Feature/implement battery wrapper (#5)
    * implemented batterywrapper
    
    * removed blank line

---
+++ imout/Core/Utils/Battery/BatteryWrapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Essentials;
+using XamarinBattery = Xamarin.Essentials.Battery;
+
+namespace imout.Core.Utils.Battery
+{
+    public class BatteryWrapper
+    {
+        public event EventHandler OnBatteryChanged;
+
+        public BatteryWrapper()
+        {
+            XamarinBattery.BatteryInfoChanged += OnChanged;
+        }
+
+        private void OnChanged(object sender, BatteryInfoChangedEventArgs e)
+        {
+            BatteryLevelChangedEventArgs evArgs = new BatteryLevelChangedEventArgs()
+            {
+                Level = e.ChargeLevel,
+                IsCharging = e.State == BatteryState.Charging
+            };
+            
+            OnChange(evArgs);
+        }
+
+        protected virtual void OnChange(BatteryLevelChangedEventArgs e)
+        {
+            EventHandler handler = OnBatteryChanged;
+            handler?.Invoke(this, e);
+        }
+    }
+}
